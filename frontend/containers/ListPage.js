99af9ce050549f2c10a276d3a075a2768c1676b4
Daniel <daniel@engfeldt.net>
2016-04-28 20:58:17 +0200

    Used blog template from bootstrap to create a quick frontpage.

--- frontend/containers/ListPage.js
+++ frontend/containers/ListPage.js
@@ -1,18 +1,80 @@
 import React from 'react';
 import req from 'axios';
 
-class Post extends React.Component {
-    render () {
-        console.log("##### render Post");
+class Footer extends React.Component{
+    render() {
+        console.log("##### render Footer");
         return (
-            <div onClick={this.props.onClick}>
-                <li>{this.props.title} - {this.props.body}</li>
+            <div>
+                <hr/>
+                <footer>
+                    <p>&copy; 2015 Company, Inc.</p>
+                </footer>
             </div>
         )
     }
 }
 
-class ListPage extends React.Component {
+class Jumbotron extends React.Component{
+    render() {
+        console.log("##### render Jumbotron");
+        return (
+            <div className="jumbotron">
+                <div className="container">
+                    <h1>Hello, world!</h1>
+                    <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+                    <p><a className="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+                </div>
+            </div>
+        )
+    }
+}
+
+class Post extends React.Component {
+    render () {
+        console.log("##### render Post");
+        return (
+            <div className="col-md-4" onClick={this.props.onClick}>
+                <h2>{this.props.title}</h2>
+                <p>{this.props.body}</p>
+                <p><a className="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+            </div>
+        )
+    }
+}
+
+class Navbar extends React.Component {
+    render () {
+        return (
+            <nav className="navbar navbar-inverse navbar-fixed-top">
+                <div className="container">
+                    <div className="navbar-header">
+                        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                            <span className="sr-only">Toggle navigation</span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                        </button>
+                        <a className="navbar-brand" href="#">Project name</a>
+                    </div>
+                    <div id="navbar" className="navbar-collapse collapse">
+                        <form className="navbar-form navbar-right">
+                            <div className="form-group">
+                                <input type="text" placeholder="Email" className="form-control"/>
+                            </div>
+                            <div className="form-group">
+                                <input type="password" placeholder="Password" className="form-control"/>
+                            </div>
+                            <button type="submit" className="btn btn-success">Sign in</button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        )
+    }
+}
+
+class PostList extends React.Component {
 
     constructor(props) {
         super(props);
@@ -27,7 +89,6 @@ class ListPage extends React.Component {
     }
 
     componentDidMount () {
-        console.log("##### componentDidMount");
         this.loadPosts();
     }
 
@@ -47,7 +108,8 @@ class ListPage extends React.Component {
 
 
     handleClick(index){
-        console.log("##### handleClick");
+        console.log("##### handleClick ("+index+")");
+        var selectedPost = this.state.posts[index];
     }
 
     render () {
@@ -55,14 +117,29 @@ class ListPage extends React.Component {
         return (
             <div>
                 {this.state.error}
-                <ul>
+                <div className="row">
                     {this.state.posts.map(function(item, i) {
                       var boundClick = this.handleClick.bind(this, i);
                       return (
                         <Post onClick={boundClick} key={i} title={item.title} body={item.body} ref={'item' + i} />
                       );
                     }, this)}
-                </ul>
+                </div>
+            </div>
+        )
+    }
+}
+
+class ListPage extends React.Component {
+
+    render () {
+        console.log("##### render page");
+        return (
+            <div className="container">
+                <Navbar/>
+                <Jumbotron/>
+                <PostList/>
+                <Footer/>
             </div>
         )
     }
