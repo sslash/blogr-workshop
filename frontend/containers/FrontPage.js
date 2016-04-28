5ffb37fd1812c833485434225e1a51c0b3e69017
Daniel <daniel@engfeldt.net>
2016-04-28 21:52:42 +0200

    Update readme and remove login fields in header

--- frontend/containers/FrontPage.js
+++ frontend/containers/FrontPage.js
@@ -57,17 +57,6 @@ class Navbar extends React.Component {
                         </button>
                         <a className="navbar-brand" href="#">Project name</a>
                     </div>
-                    <div id="navbar" className="navbar-collapse collapse">
-                        <form className="navbar-form navbar-right">
-                            <div className="form-group">
-                                <input type="text" placeholder="Email" className="form-control"/>
-                            </div>
-                            <div className="form-group">
-                                <input type="password" placeholder="Password" className="form-control"/>
-                            </div>
-                            <button type="submit" className="btn btn-success">Sign in</button>
-                        </form>
-                    </div>
                 </div>
             </nav>
         )
