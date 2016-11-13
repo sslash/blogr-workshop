2a4d38ea8090d0a86f72a3281bd0dff548caf51c
Daniel <daniel@engfeldt.net>
2016-11-13 20:13:22 +0100

    add BABEL_ENV and NODE_ENV for both react and server

--- react/webpack.prod.config.js
+++ react/webpack.prod.config.js
@@ -8,7 +8,8 @@ module.exports = Object.assign(config, {
     plugins: [
         new webpack.DefinePlugin({
             'process.env': {
-              'NODE_ENV': JSON.stringify('production')
+              'NODE_ENV': JSON.stringify('production'),
+              'BABEL_ENV': JSON.stringify('production')
             }
         })
     ]
--- server/webpack.dev.config.js
+++ server/webpack.dev.config.js
@@ -1,4 +1,12 @@
 'use strict';
 var config = require('./webpack.config');
 module.exports = Object.assign(config, {
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+              'NODE_ENV': JSON.stringify('development'),
+              'BABEL_ENV': JSON.stringify('development')
+            }
+        })
+    ]
 });
