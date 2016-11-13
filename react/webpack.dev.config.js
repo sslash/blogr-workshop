baec51db81b997d55abacb64342c6e2162026c39
Daniel <daniel@engfeldt.net>
2016-11-13 20:37:04 +0100

    remove unused dev-server config

--- react/webpack.dev.config.js
+++ react/webpack.dev.config.js
@@ -20,15 +20,5 @@ module.exports = Object.assign(config, {
               'BABEL_ENV': JSON.stringify('production')
             }
         })
-    ],
-    devServer: {
-         port: 3001,
-         contentBase: '/',
-         historyApiFallback: true,
-         proxy: {
-           '/api': {
-             target: 'http://127.0.0.1:3000'
-           }
-         }
-     }
+    ]
 });
