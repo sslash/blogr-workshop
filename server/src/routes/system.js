7a12b6aedc339d3cce312260f54462db3130de51
Daniel <daniel@engfeldt.net>
2016-11-20 12:33:15 +0100

    added build_tag to defineplugin and system info for more info from jenkins build

--- server/src/routes/system.js
+++ server/src/routes/system.js
@@ -5,22 +5,15 @@ var router = express.Router();
 const info = {
   version: __VERSION__,
   hash: __HASH__,
-  build: __BUILD__
+  build_number: __BUILD_NUMBER__,
+  build_tag: __BUILD_TAG__,
 }
 
 router.get('/system/ping', function(req, res, next) {
     res.json({pong: true});
 });
 
-router.get('/system/version', function(req, res, next) {
-    res.json({version: info.version});
-});
-
-router.get('/system/hash', function(req, res, next) {
-    res.json({hash: info.hash});
-});
-
-router.get('/system', function(req, res, next) {
+router.get('/system/info', function(req, res, next) {
     res.json(Object.assign({}, info, process.env));
 });
 
--- server/webpack.prod.config.js
+++ server/webpack.prod.config.js
@@ -12,19 +12,13 @@ var commitHash = require('child_process')
 
 var versionFromPackage = require("./package.json").version;
 
-console.info("Build with environment for production");
-console.info("BABEL_ENV", process.env.BABEL_ENV);
-console.info("NODE_ENV", process.env.NODE_ENV);
-console.info("HASH", commitHash);
-console.info("VERSION", versionFromPackage);
-console.info("BUILD", process.env.BUILD);
-
 module.exports = Object.assign(config, {
     plugins: [
         new webpack.DefinePlugin({
             '__VERSION__': JSON.stringify(versionFromPackage),
             '__HASH__': JSON.stringify(commitHash),
-            '__BUILD__': JSON.stringify(process.env.BUILD),
+            '__BUILD_NUMBER__': JSON.stringify(process.env.BUILD_NUMBER),
+            '__BUILD_TAG__': JSON.stringify(process.env.BUILD_TAG),
             'process.env': {
               'NODE_ENV': JSON.stringify('production'),
               'BABEL_ENV': JSON.stringify('production')
