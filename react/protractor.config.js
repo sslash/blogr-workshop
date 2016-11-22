70fe0502ecd0d693b4fdd2c6b68677de47e796c4
Daniel <daniel@engfeldt.net>
2016-11-22 19:37:42 +0100

    change brower to phantomjs

--- react/protractor.config.js
+++ react/protractor.config.js
@@ -4,7 +4,9 @@
 exports.config = {
   specs: ['./test/e2e/**/*.spec.js'],
   capabilities: {
-    browserName: 'chrome'
+      browserName: 'phantomjs',
+      'phantomjs.binary.path': require('phantomjs-prebuilt').path,
+      'phantomjs.cli.args': ['--web-security=false', '--ignore-ssl-errors=true', '--webdriver-loglevel=DEBUG'],
   },
   baseUrl: 'http://localhost:3000',
   frameworks: ['mocha', 'chai'],
