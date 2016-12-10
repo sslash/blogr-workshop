61f82ca4966ab461d190168d895e141777b0424c
Daniel <daniel@engfeldt.net>
2016-12-10 20:48:00 +0100

    forgot to fix for production webpack also

--- server/webpack.prod.config.js
+++ server/webpack.prod.config.js
@@ -6,13 +6,15 @@ var fs = require('fs');
 var config = require('./webpack.config');
 var git = require('git-rev-sync');
 var version = require('./package.json').version;
+var childProcess = require('child_process'),
+const GIT_TAG = childProcess.execSync('git describe --exact-match || echo \"no tag found\"').toString();
 
 module.exports = Object.assign(config, {
     plugins: [
         new webpack.DefinePlugin({
             '__VERSION__': JSON.stringify(version),
             '__HASH__': JSON.stringify(git.short()),
-            '__TAG__': JSON.stringify(git.tag()),
+            '__TAG__': JSON.stringify(GIT_TAG),
             '__BUILD_NUMBER__': JSON.stringify(process.env.BUILD_NUMBER),
             '__BUILD_TAG__': JSON.stringify(process.env.BUILD_TAG),
             '__BRANCH__': JSON.stringify(git.branch()),
