81b6dd378510fa3b1501596e4cc9e5b24e1621a7
Daniel Engfeldt <daniel@engfeldt.net>
2017-03-26 18:47:10 +0200

    update db address for prod.

--- server/src/config/config.production.js
+++ server/src/config/config.production.js
@@ -1,6 +1,6 @@
 var config = require('./config.defaults');
 
-config.db.host = 'db-2.dragon.lan';
+config.db.host = 'p-db-01.dragon.lan';
 config.db.user = 'blogr';
 config.db.password = 'password1';
 
