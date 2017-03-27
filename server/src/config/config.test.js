ba6cd767817b80dc66b53e5fe121f62e1aef8d4e
Daniel <daniel@engfeldt.net>
2017-03-27 20:46:34 +0200

    rename dev to development

--- server/src/config/config.test.js
+++ server/src/config/config.test.js
@@ -1,6 +1,6 @@
 var config = require('./config.defaults');
 
-config.db.host = 'd-db-01.dragon.lan';
+config.db.host = 't-db-01.dragon.lan';
 config.db.user = 'blogr';
 config.db.password = 'password1';
 
--- server/webpack.test.config.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-var config = require('./webpack.config');
-module.exports = config;
